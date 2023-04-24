4bdc458e8daa75f5a0bc4077c503274cdb241326
Theresa <t.neubert@live.de>
2023-04-24 09:36:20 +0200

    JDoc to make things clear

--- UTM/Band.java
+++ UTM/Band.java
@@ -1,10 +1,17 @@
 
+
+/*
+ * Diese Klasse repräsentiert das Band der TM
+ */
 public class Band {
     // Das ist mein Teststring um die Maschine zu testen
     private String band;
     private static final String TESTSTRING = "010010001010011000101010010110001001001010011000100010001010111101010";
     private int headPosition = 0;
 
+    /**
+     * Default Konstruktor um das Band zu testen
+     */
     public Band() {
         InputData inputData = new InputData();
         inputData.splitInput(TESTSTRING);
@@ -12,22 +19,41 @@ public class Band {
         band = inputData.splitInput(TESTSTRING)[1];
     }
 
+    /**
+     * Erhalte das aktuelle band
+     * @return
+     */
     public String getBand() {
         return this.band;
     }
 
+    /**
+     * Konstruktor um Band aus spezifischem String zu erstellen
+     * @param withBand
+     */
     public Band(String withBand) {
         band = withBand;
     }
 
+    /**
+     * Gebe das Band mit 15 Zeichen vor und nach dem Leseschreiblopf aus
+     */
     public void printBand() {
         printBand(15);
     }
 
+    /**
+     * Sagt ob das Band zu Ende ist
+     * @return true = band ist zu ende false = band ist nicht zu ende
+     */
     public boolean isEnd() {
         return headPosition == band.length() - 1;
     }
 
+    /**
+     * Erstellt die optische Darstellung auf der Konsole des bandes
+     * @param padding
+     */
     public void printBand(int padding) {
         StringBuilder sb = new StringBuilder();
         StringBuilder sbHead = new StringBuilder();
@@ -73,28 +99,44 @@ public class Band {
         System.out.println(sbHead.toString());
     }
 
+    /**
+     * Ersetzt/überschreibt das aktuell auf dem band gelesene Zeichen
+     * @param c
+     */
     public void write(char c) {
         StringBuilder sb = new StringBuilder(band);
         sb.setCharAt(headPosition, c);
         band = sb.toString();
     }
 
+    /**
+     * Liest das aktuelle Zeichen auf dem Band dort wo sich der Lese-Schreibkopf befindet
+     * @return
+     */
     public char read() {
         return band.charAt(headPosition);
     }
 
+    /**
+     * Bewegt den Lese-Schreibkopf nach links
+     */
     public void moveHeadLeft() {
         if (headPosition > 0) {
             headPosition--;
         }
     }
 
+    /**
+     * Bewegt den Leseschreibkopf nach rechts
+     */
     public void moveHeadRight() {
         if (headPosition < band.length() - 1) {
             headPosition++;
         }
     }
 
+
+    //Das ist nur zum Testen, kann sonst ignoriert werden. das Prograamm wird in der Klasse UTM.java gestartet
     public static void main(String[] args) {
         Band tb = new Band();
         tb.printBand();
--- UTM/StateMachine.java
+++ UTM/StateMachine.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.management.RuntimeErrorException;
 
 public class StateMachine {
     private Band band;
@@ -11,16 +10,30 @@ public class StateMachine {
     private String acceptedState = "Q2";
     private int step = 0;
 
+    //Hier bauen wir quasi unsere State Machine zusammen um später durch die verschiedenen Zustände iterieren zu können,
+    // die wir aus dem Bandinput lesen. SplittetInput 0 enthält die eingegebene TM und SplittetInput 1 den String der als 
+    //Eingabe auf das Band geschrieben wird
     public StateMachine(String fullBand) {
+        //Eingegebene binär codierte TM inkls. InputString
         InputData bandInput = new InputData();
-        String[] splittedInout = bandInput.splitInput(fullBand);
-        this.band = new Band(splittedInout[1]);
-        bandInput.readTransitions(splittedInout[0]);
+        // Eingabe wird zwei geteilt in TM und Input
+        String[] splittedInput = bandInput.splitInput(fullBand);
+        //Erstellt das Band auf dem die TM arbeitet mit input String
+        this.band = new Band(splittedInput[1]);
+        //Die Übergangsfunktionen werden aus der binär codierten TM ersttellt
+        bandInput.readTransitions(splittedInput[0]);
+        //Das ist aktuell noch zum überprüfen welche Übergangsfunktionen wir haben
         System.out.println(bandInput.getTransitionList());
+        //hier wird das binäre in Menschlich verständlich notierte übergangsfunktionen übersetzt
         InputTranslator it = new InputTranslator(bandInput.getTransitionList());
+        //hier werden die Übergnagsfunktionen aus einer ArrayList in eine HashMap übersetzt
         convertArrayListToHashMap(it.getAndTranslateBandInput());
     }
 
+    /**
+     * @param translatedTrasitions das ist ein zweidimensionales array das alle übergangsfunktionen enthält
+     * @return die HashMap in diesem Format: {Q3_1=Q2,0,R, Q3_0=Q1,1,L, Q1_1=Q3,0,R, Q3__=Q3,0,L}
+     */
     public HashMap<String, String> convertArrayListToHashMap(ArrayList<ArrayList<String>> translatedTrasitions) {
         transitionMap = new HashMap<>();
         for(ArrayList<String> entry : translatedTrasitions) {
@@ -30,11 +43,16 @@ public class StateMachine {
             transitionMap.put(key, value);
 
         }
-
+        System.out.println(transitionMap.toString());
         return transitionMap;
         
     } 
 
+    /**
+     * Hier werden die Zustände anhand des Bandes und der transitionMap umgesetzt
+     * @param c das ist der charakter den wir vom Band lesen
+     * @throws StateMachienException
+     */
     public void doTransition(char c) throws StateMachienException {
         if(state == acceptedState){
             System.out.println("Akzeptierter Zustand erreicht.");
@@ -65,6 +83,9 @@ public class StateMachine {
         }
     }
 
+    /**
+     * Hier geben wir aus in welchem Zustand wir uns aktuell befinden und wie viele Steps wir bisher gemacht haben
+     */
     public void printMachieneState() {
         System.out.println("Current State: "+this.state);
         System.out.println("Current Step: "+this.step);
@@ -73,7 +94,7 @@ public class StateMachine {
 
     /**
      * 
-     * @return true if the band is at its enda
+     * @return true wenn das Band zu ende ist
      * @throws StateMachienException
      */
     public boolean step() throws StateMachienException {
